ebb85d66f620ea9353efdf31d5b0c258ae47747e
Malay <malaymehta26@gmail.com>
2021-07-27 00:40:59 +0530

    Delete hybris/bin/platform directory

--- hybris/bin/platform/bootstrap/gensrc/com/hybris/datahub/model/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Contains generated models for each type of com.hybris.datahub.jalo package.
- */
-package com.hybris.datahub.model;
-
--- hybris/bin/platform/ext/hac/web/webroot/WEB-INF/jsp/decorators/_nav.jsp
+++
@@ -1,3 +0,0 @@
-<%@ taglib prefix="hac" uri="/WEB-INF/custom.tld" %> 
-
-<hac:tabbed-menu subtabCssClass="span-24 last"/>
